55bb096b794f7ce8cdfd2d4e3d10d9422adcfe06
Matvey <matvey2k@gmail.com>
2020-09-08 10:27:30 +0300

    Added business logic level

---
+++ backend/net-food/BLL.net-food/Infrastructure/ServiceModule.cs
@@ -0,0 +1,19 @@
+﻿using DAL.net_food;
+using DAL.net_food.Interfaces;
+using Ninject.Modules;
+
+namespace BLL.net_food.Infrastructure
+{
+    public class ServiceModule : NinjectModule
+    {
+        private string connectionString;
+        public ServiceModule(string connection)
+        {
+            connectionString = connection;
+        }
+        public override void Load()
+        {
+            Bind<IUnitOfWork>().To<UnitOfWork>().WithConstructorArgument(connectionString);
+        }
+    }
+}
---
+++ backend/net-food/BLL.net-food/Services/DrinkService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using BLL.net_food.Interfaces;
+using DAL.net_food.Entities;
+using DAL.net_food.Interfaces;
+
+namespace BLL.net_food.Services
+{
+    public class DrinkService : IDrinkService
+    {
+        IUnitOfWork Database { get; set; }
+
+        public DrinkService(IUnitOfWork uow)
+        {
+            Database = uow;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Drink> Get()
+        {
+            return Database.Drinks.Get();
+        }
+
+        public Drink Get(int id)
+        {
+            return Database.Drinks.Get(id);
+        }
+
+        public void Order(Drink orderDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Drink drink)
+        {
+            Database.Drinks.Update(drink);
+        }
+    }
+}
