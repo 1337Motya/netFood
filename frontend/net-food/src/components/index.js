31e5fb9d79365687217536a26bdfb52f346cc709
Matvey <matvey2k@gmail.com>
2020-12-14 00:26:01 +0300

    Added new exports

--- frontend/net-food/src/components/index.js
+++ frontend/net-food/src/components/index.js
@@ -2,6 +2,14 @@ export { default as Header } from "./Header";
 export { default as Categories } from "./Categories";
 export { default as Button } from "./Button";
 export { default as SortPopup } from "./SortPopup";
-export { default as PizzaBlock } from "./PizzaBlock/PizzaBlock";
-export { default as LoadingBlock } from "./PizzaBlock/LoadingBlock";
+export { default as PizzaBlock } from "./ItemBlocks/PizzaBlock";
+export { default as SnackBlock } from "./ItemBlocks/SnackBlock";
+export { default as DrinkBlock } from "./ItemBlocks/DrinkBlock";
+export { default as DessertBlock } from "./ItemBlocks/DessertBlock";
+export { default as LoadingBlock } from "./ItemBlocks/LoadingBlock";
 export { default as CartItem } from "./CartItem";
+export {default as SnackAddBlock } from "./AdminBlocks/SnackAddBlock"
+export {default as SnackEditBlock } from "./AdminBlocks/SnackEditBlock"
+export {default as DessertEditBlock } from "./AdminBlocks/DessertEditBlock"
+export {default as DrinkEditBlock } from "./AdminBlocks/DrinkEditBlock"
+export {default as PizzaEditBlock } from "./AdminBlocks/PizzaEditBlock"
