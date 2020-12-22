a412f5723d08f15187bf7d582507f1c4888038ec
Matvey.Kiselev <matvey2k@gmail.com>
2020-12-22 23:59:17 +0300

    fixed problems with props

--- frontend/net-food/src/components/AdminBlocks/DessertEditBlock.jsx
+++ frontend/net-food/src/components/AdminBlocks/DessertEditBlock.jsx
@@ -1,19 +1,35 @@
 import React from "react";
-import classNames from "classnames";
-import PropTypes from "prop-types";
 import Button from "../Button";
+import { ReactComponent as PlusIcon } from '../../assets/img/plus.svg'
 
 function DessertEditBlock({
   id,
   name,
   categoryId,
   onDelete,
+  onEdit,
   description,
   imageUrl,
   price,
 }) {
+  const [nameEdit, setName] = React.useState(name);
+  const [descriptionEdit, setDescription] = React.useState(description);
+  const [imageUrlEdit, setImageUrl] = React.useState(imageUrl);
+  const [priceEdit, setPrice] = React.useState(price);
+
   const deleteDessert = () => {
     onDelete("desserts", id);
+  };
+
+  const handleSubmit = () => {
+    var newItem = {
+      id: id,
+      name: nameEdit,
+      description: descriptionEdit,
+      ImageUrl: imageUrl,
+      price: priceEdit,
+    }
+    onEdit("desserts", newItem, id);
   };
 
   return (
@@ -23,30 +39,24 @@ function DessertEditBlock({
           onClick={deleteDessert}
           className="button button--outline button--circle"
         >
-          <svg
-            width="10"
-            height="10"
-            viewBox="0 0 10 10"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z"
-              fill="#EB5A1E"
-            />
-            <path
-              d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"
-              fill="#EB5A1E"
-            />
-          </svg>
+          <PlusIcon />
         </Button>
       </div>
       <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
-      <h4 className="pizza-block__title">{name}</h4>
-      <span className="pizza-block__description">{description}</span>
+      <div className="link-block">
+        <input type="text" className="item-input" placeholder="Название" value={imageUrlEdit} onChange={(e) => setImageUrl(e.target.value)} />
+      </div>
+      <div>
+        <textarea className="item-edit-textarea textarea-name" placeholder="Имя" type="text" value={nameEdit} onChange={(e) => setName(e.target.value)} />
+      </div>
+      <div>
+        <textarea className="item-edit-textarea textarea-description" placeholder="Описание" type="text" value={descriptionEdit} onChange={(e) => setDescription(e.target.value)} />
+      </div>
       <div className="pizza-block__bottom">
-        <div className="pizza-block__price">{price} р.</div>
-        <Button className="button--add" outline>
+        <div className="pizza-block__price">
+          <input placeholder="0.0" className="item-edit-input input-price-edit" type="text" value={priceEdit} onChange={(e) => setPrice(e.target.value)} /> р.
+        </div>
+        <Button onClick={handleSubmit} className="button--add" outline>
           <span>Сохранить</span>
         </Button>
       </div>
